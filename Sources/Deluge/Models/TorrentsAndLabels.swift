8dee7752695987460f8c0f76e3cd20566061ba65
NinjaLikesCheez <NinjaLikesCheez@users.noreply.github.com>
2026-07-08 19:19:39 +0200

    Expand Torrent status field coverage
    Adds ratio, isFinished, queue, isPrivate, comment, creator, numFiles,
    filePriorities, maxDownloadSpeed, maxUploadSpeed, activeTime,
    completedTime, finishedTime, trackerStatus, tracker, trackerHost,
    moveCompletedPath, autoManaged, and distributedCopies to Torrent,
    PropertyKeys, and the web.update_ui decoding path.
    
    Fixes #13

--- Sources/Deluge/Models/TorrentsAndLabels.swift
+++ Sources/Deluge/Models/TorrentsAndLabels.swift
@@ -34,41 +34,79 @@ public struct TorrentsAndLabels: Decodable, Sendable {
 /// Shared by `web.update_ui` (via `TorrentsAndLabels`) and `core.get_torrent_status`/`core.get_torrents_status`,
 /// all of which return the same field names but never include the torrent's hash in the payload itself.
 struct UnhashedTorrent: Decodable, Sendable {
+	let activeTime: TimeInterval?
+	let autoManaged: Bool?
+	let completedTime: TimeInterval?
+	let comment: String?
+	let creator: String?
 	let dateAdded: Date?
+	let distributedCopies: Float?
 	let downloaded: Int64?
 	let downloadPath: String?
 	let downloadRate: Int64?
 	let eta: TimeInterval?
+	let filePriorities: [Int]?
+	let finishedTime: TimeInterval?
+	let isFinished: Bool?
+	let isPrivate: Bool?
 	let label: String?
+	let maxDownloadSpeed: Float?
+	let maxUploadSpeed: Float?
+	let moveCompletedPath: String?
 	let name: String?
+	let numFiles: Int?
 	let peers: Int?
 	let progress: Float?
+	let queue: Int?
+	let ratio: Float?
 	let seeds: Int?
 	let seedingTime: TimeInterval?
 	let size: Int64?
 	let state: Torrent.State?
 	let totalPeers: Int?
 	let totalSeeds: Int?
+	let tracker: String?
+	let trackerHost: String?
+	let trackerStatus: String?
 	let trackers: [Tracker]?
 	let uploaded: Int64?
 	let uploadRate: Int64?
 
 	enum CodingKeys: String, CodingKey {
+		case activeTime = "active_time"
+		case autoManaged = "auto_managed"
+		case completedTime = "completed_time"
+		case comment
+		case creator
 		case dateAdded = "time_added"
+		case distributedCopies = "distributed_copies"
 		case downloaded = "total_done"
 		case downloadPath = "download_location"
 		case downloadRate = "download_payload_rate"
 		case eta
+		case filePriorities = "file_priorities"
+		case finishedTime = "finished_time"
+		case isFinished = "is_finished"
+		case isPrivate = "private"
 		case label
+		case maxDownloadSpeed = "max_download_speed"
+		case maxUploadSpeed = "max_upload_speed"
+		case moveCompletedPath = "move_completed_path"
 		case name
+		case numFiles = "num_files"
 		case peers = "num_peers"
 		case progress
+		case queue
+		case ratio
 		case seeds = "num_seeds"
 		case seedingTime = "seeding_time"
 		case size = "total_size"
 		case state
 		case totalPeers = "total_peers"
 		case totalSeeds = "total_seeds"
+		case tracker
+		case trackerHost = "tracker_host"
+		case trackerStatus = "tracker_status"
 		case trackers
 		case uploaded = "total_uploaded"
 		case uploadRate = "upload_payload_rate"
@@ -84,24 +122,43 @@ struct UnhashedTorrent: Decodable, Sendable {
 			dateAdded = nil
 		}
 
+		activeTime = try container.decodeIfPresent(TimeInterval.self, forKey: .activeTime)
+		autoManaged = try container.decodeIfPresent(Bool.self, forKey: .autoManaged)
+		completedTime = try container.decodeIfPresent(TimeInterval.self, forKey: .completedTime)
+		comment = try container.decodeIfPresent(String.self, forKey: .comment)
+		creator = try container.decodeIfPresent(String.self, forKey: .creator)
+		distributedCopies = try container.decodeIfPresent(Float.self, forKey: .distributedCopies)
 		downloaded = try container.decodeIfPresent(Int64.self, forKey: .downloaded)
 		downloadPath = try container.decodeIfPresent(String.self, forKey: .downloadPath)
 		downloadRate = try container.decodeIfPresent(Int64.self, forKey: .downloadRate)
 		eta = try container.decodeIfPresent(TimeInterval.self, forKey: .eta)
+		filePriorities = try container.decodeIfPresent([Int].self, forKey: .filePriorities)
+		finishedTime = try container.decodeIfPresent(TimeInterval.self, forKey: .finishedTime)
+		isFinished = try container.decodeIfPresent(Bool.self, forKey: .isFinished)
+		isPrivate = try container.decodeIfPresent(Bool.self, forKey: .isPrivate)
 		label = try container.decodeIfPresent(String.self, forKey: .label)
+		maxDownloadSpeed = try container.decodeIfPresent(Float.self, forKey: .maxDownloadSpeed)
+		maxUploadSpeed = try container.decodeIfPresent(Float.self, forKey: .maxUploadSpeed)
+		moveCompletedPath = try container.decodeIfPresent(String.self, forKey: .moveCompletedPath)
 		name = try container.decodeIfPresent(String.self, forKey: .name)
+		numFiles = try container.decodeIfPresent(Int.self, forKey: .numFiles)
 		peers = try container.decodeIfPresent(Int.self, forKey: .peers)
 		if let progressValue = try container.decodeIfPresent(Float.self, forKey: .progress) {
 			progress = (progressValue / 100)
 		} else {
 			progress = nil
 		}
+		queue = try container.decodeIfPresent(Int.self, forKey: .queue)
+		ratio = try container.decodeIfPresent(Float.self, forKey: .ratio)
 		seeds = try container.decodeIfPresent(Int.self, forKey: .seeds)
 		seedingTime = try container.decodeIfPresent(TimeInterval.self, forKey: .seedingTime)
 		size = try container.decodeIfPresent(Int64.self, forKey: .size)
 		state = try container.decodeIfPresent(Torrent.State.self, forKey: .state)
 		totalPeers = try container.decodeIfPresent(Int.self, forKey: .totalPeers)
 		totalSeeds = try container.decodeIfPresent(Int.self, forKey: .totalSeeds)
+		tracker = try container.decodeIfPresent(String.self, forKey: .tracker)
+		trackerHost = try container.decodeIfPresent(String.self, forKey: .trackerHost)
+		trackerStatus = try container.decodeIfPresent(String.self, forKey: .trackerStatus)
 		trackers = try container.decodeIfPresent([Tracker].self, forKey: .trackers)
 		uploaded = try container.decodeIfPresent(Int64.self, forKey: .uploaded)
 		uploadRate = try container.decodeIfPresent(Int64.self, forKey: .uploadRate)
@@ -111,22 +168,41 @@ struct UnhashedTorrent: Decodable, Sendable {
 extension Torrent {
 	init(hash: String, torrent: UnhashedTorrent) {
 		self = .init(
+			activeTime: torrent.activeTime,
+			autoManaged: torrent.autoManaged,
+			completedTime: torrent.completedTime,
+			comment: torrent.comment,
+			creator: torrent.creator,
 			dateAdded: torrent.dateAdded,
+			distributedCopies: torrent.distributedCopies,
 			downloaded: torrent.downloaded,
 			downloadPath: torrent.downloadPath,
 			downloadRate: torrent.downloadRate,
 			eta: torrent.eta,
+			filePriorities: torrent.filePriorities,
+			finishedTime: torrent.finishedTime,
 			hash: hash,
+			isFinished: torrent.isFinished,
+			isPrivate: torrent.isPrivate,
 			label: torrent.label,
+			maxDownloadSpeed: torrent.maxDownloadSpeed,
+			maxUploadSpeed: torrent.maxUploadSpeed,
+			moveCompletedPath: torrent.moveCompletedPath,
 			name: torrent.name,
+			numFiles: torrent.numFiles,
 			peers: torrent.peers,
 			progress: torrent.progress,
+			queue: torrent.queue,
+			ratio: torrent.ratio,
 			seeds: torrent.seeds,
 			seedingTime: torrent.seedingTime,
 			size: torrent.size,
 			state: torrent.state,
 			totalPeers: torrent.totalPeers,
 			totalSeeds: torrent.totalSeeds,
+			tracker: torrent.tracker,
+			trackerHost: torrent.trackerHost,
+			trackerStatus: torrent.trackerStatus,
 			trackers: torrent.trackers,
 			uploaded: torrent.uploaded,
 			uploadRate: torrent.uploadRate
